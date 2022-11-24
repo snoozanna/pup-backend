175ef99284c618f818a179fcc3f638fc5b0a1fb8
suzannahurst <hurstsuzanna@gmail.com>
2022-11-24 17:23:21 +0000

    separated out database env

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -17,17 +17,22 @@
 const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+  // const { host, port, database, user, password } = parse(env("DATABASE_URL"));
 
   return {
     connection: {
       client: "postgres",
       connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapi"),
+        user: env("DATABASE_USERNAME", "strapi"),
+        password: env("DATABASE_PASSWORD", "strapi"),
+        // host,
+        // port,
+        // database,
+        // user,
+        // password,
         ssl: {
           rejectUnauthorized: false,
         },
