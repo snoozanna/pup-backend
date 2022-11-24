dc7902d6da32849e88f6613f4f8c7ed7eaeeddea
suzannahurst <hurstsuzanna@gmail.com>
2022-11-24 17:48:58 +0000

    external url

--- config/database.js
+++ config/database.js
@@ -1,34 +1,15 @@
-// const path = require('path');
+const path = require("path");
 
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'sqlite',
-//     connection: {
-//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-//     },
-//     useNullAsDefault: true,
-//   },
-// });
-
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: "sqlite",
     connection: {
-      client: "postgres",
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      },
-      debug: false,
+      filename: path.join(
+        __dirname,
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
     },
-  };
-};
+    useNullAsDefault: true,
+  },
+});
