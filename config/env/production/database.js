abfd2814f2eaaf329cfcf8c2c47ceb5c06474dff
suzannahurst <hurstsuzanna@gmail.com>
2022-11-24 19:25:54 +0000

    copied file from example repo

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,41 +1,17 @@
-// const path = require("path");
-
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: "sqlite",
-//     connection: {
-//       filename: path.join(
-//         __dirname,
-//         "..",
-//         env("DATABASE_FILENAME", ".tmp/data.db")
-//       ),
-//     },
-//     useNullAsDefault: true,
-//   },
-// });
-
 const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
-  // const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
 
   return {
     connection: {
       client: "postgres",
       connection: {
-        host: env("DATABASE_HOST", "127.0.0.1"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
-        // host,
-        // port,
-        // database,
-        // user,
-        // password,
-        ssl: {
-          rejectUnauthorized: false,
-        },
+        host,
+        port,
+        database,
+        user,
+        password
       },
       debug: false,
     },
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,3 @@
 module.exports = ({ env }) => ({
-  url: env("https://strapi-v1ph.onrender.com"),
+  url: env("RENDER_EXTERNAL_URL"),
 });
