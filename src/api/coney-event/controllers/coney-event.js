3a87dc30b4f1a5fa85ad141be58d86a83b1177de
suzannahurst <hurstsuzanna@gmail.com>
2023-04-24 16:00:47 +0100

    remove events

--- src/api/coney-event/controllers/coney-event.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * coney-event controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::coney-event.coney-event');
--- src/api/coney-event/services/coney-event.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * coney-event service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::coney-event.coney-event');
