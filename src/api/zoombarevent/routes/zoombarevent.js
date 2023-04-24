30caef869e92feb1c3a27258867af1231d8e8914
suzannahurst <hurstsuzanna@gmail.com>
2023-04-24 16:06:55 +0100

    events

---
+++ src/api/zoombarevent/routes/zoombarevent.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * zoombarevent router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::zoombarevent.zoombarevent');
---
+++ src/api/zoombarevent/services/zoombarevent.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * zoombarevent service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::zoombarevent.zoombarevent');
